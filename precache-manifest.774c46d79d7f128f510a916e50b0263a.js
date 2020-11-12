1ea50a27420f20fb153f30f5b52779ba3b77c867
Aditya-1127 <71876753+Aditya-1127@users.noreply.github.com>
2020-11-12 11:03:54 +0530

    Updates

---
+++ precache-manifest.774c46d79d7f128f510a916e50b0263a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7983fd290d2f2ff00202e4efe961b6ab",
+    "url": "/ZapsDemo/index.html"
+  },
+  {
+    "revision": "4040625e3dd815d84fdd",
+    "url": "/ZapsDemo/static/css/2.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "24a437ef5ece45c50701",
+    "url": "/ZapsDemo/static/css/main.48bb78a0.chunk.css"
+  },
+  {
+    "revision": "4040625e3dd815d84fdd",
+    "url": "/ZapsDemo/static/js/2.737c6934.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/ZapsDemo/static/js/2.737c6934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24a437ef5ece45c50701",
+    "url": "/ZapsDemo/static/js/main.3922d23a.chunk.js"
+  },
+  {
+    "revision": "773c619be70410facbae",
+    "url": "/ZapsDemo/static/js/runtime-main.b110f6cb.js"
+  }
+]);
